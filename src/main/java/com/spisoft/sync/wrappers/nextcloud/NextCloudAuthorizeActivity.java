d3348c6a8f371ab16d4574c6649ddff51196dcf0
Phoenamandre <phoenamandre@gmail.com>
2018-01-24 12:40:45 +0100

    new authorize with server choice

--- src/main/java/com/spisoft/sync/wrappers/nextcloud/NextCloudAuthorizeActivity.java
+++ src/main/java/com/spisoft/sync/wrappers/nextcloud/NextCloudAuthorizeActivity.java
@@ -3,12 +3,14 @@ package com.spisoft.sync.wrappers.nextcloud;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.spisoft.sync.Log;
 import com.spisoft.sync.R;
 
-public class NextCloudAuthorizeActivity extends AppCompatActivity implements View.OnClickListener {
+public class NextCloudAuthorizeActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     public static final String EXTRA_ACCOUNT_ID="account_id";
     private View mConnectButton;
     private EditText mPasswordInput;
@@ -16,6 +18,8 @@ public class NextCloudAuthorizeActivity extends AppCompatActivity implements Vie
     private EditText mUsernameInput;
     private View mCancelButton;
     private int mAccountId;
+    private Spinner mServerSpinner;
+    private View mRemoteInputContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +27,14 @@ public class NextCloudAuthorizeActivity extends AppCompatActivity implements Vie
         setContentView(R.layout.activity_next_cloud_authorize);
         mAccountId = getIntent().getIntExtra(EXTRA_ACCOUNT_ID, -1);
         mConnectButton = findViewById(R.id.connect_button);
+        mServerSpinner = findViewById(R.id.server_spinner);
         mCancelButton = findViewById(R.id.cancel_button);
         mRemoteInput = findViewById(R.id.input_remote);
+        mRemoteInputContainer = findViewById(R.id.input_remote_layout);
         mUsernameInput = findViewById(R.id.input_username);
         mPasswordInput = findViewById(R.id.input_password);
         mConnectButton.setOnClickListener(this);
+        mServerSpinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -45,5 +52,33 @@ public class NextCloudAuthorizeActivity extends AppCompatActivity implements Vie
             setResult(RESULT_CANCELED);
             finish();
         }
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String [] entries = getResources().getStringArray(R.array.nextcloud_array);
+        String [] values = getResources().getStringArray(R.array.nextcloud_values);
+        String [] full =  getResources().getStringArray(R.array.nextcloud_full_instances);
+        boolean isFull = false;
+        for(String f : full){
+            if(entries[i].equals(f)) {
+                isFull = true;
+                break;
+            }
+        }
+        if(i<values.length){
+            mRemoteInputContainer.setVisibility(View.GONE);
+            mRemoteInput.setText(values[i]);
+        }
+        else{
+            mRemoteInputContainer.setVisibility(View.VISIBLE);
+            mRemoteInput.setText("");
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
